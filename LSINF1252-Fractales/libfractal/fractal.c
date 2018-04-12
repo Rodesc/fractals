0ee066986917ec7197e1064953888504c6152825
Rodesc <rodolphe.deschaetzen.dev@gmail.com>
2018-04-12 10:24:02 +0200

    Implementation getters setters et fractal_free

--- LSINF1252-Fractales/libfractal/fractal.c
+++ LSINF1252-Fractales/libfractal/fractal.c
@@ -17,45 +17,47 @@ struct fractal *fractal_new(const char *name, int width, int height, double a, d
 void fractal_free(struct fractal *f)
 {
     /* TODO */
+    *f = NULL;
 }
 
 const char *fractal_get_name(const struct fractal *f)
 {
     /* TODO */
-    return NULL;
+    return f->name;
 }
 
 int fractal_get_value(const struct fractal *f, int x, int y)
 {
     /* TODO */
-    return 0;
+    return f->value[x][y];
 }
 
 void fractal_set_value(struct fractal *f, int x, int y, int val)
 {
     /* TODO */
+    f->value[x][y] = val;
 }
 
 int fractal_get_width(const struct fractal *f)
 {
     /* TODO */
-    return 0;
+    return f->width;
 }
 
 int fractal_get_height(const struct fractal *f)
 {
     /* TODO */
-    return 0;
+    return f->height;
 }
 
 double fractal_get_a(const struct fractal *f)
 {
     /* TODO */
-    return 0;
+    return f->a;
 }
 
 double fractal_get_b(const struct fractal *f)
 {
     /* TODO */
-    return 0;
+    return f->b;
 }
