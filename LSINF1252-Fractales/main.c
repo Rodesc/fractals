06fe9e77efe0ebc1ed37030216fb81bb697cfd58
Rodesc <rodolphe.deschaetzen.dev@gmail.com>
2018-04-18 15:32:01 +0200

    Modif MakeFile + main: CompilationOK

--- LSINF1252-Fractales/main.c
+++ LSINF1252-Fractales/main.c
@@ -14,13 +14,16 @@ int main(){
 /*
 *	Ebauche de fonction qui calcule la fractale en prenant en compte la propriété de symétrie des fractales de Julia. 
 */
-void compute_value(struct fractal *f){
-
-	for(int i = 0; i < f->width/2; i++ ){
-		for (int j = 0; j < f->height/2; j++){
-			int val = fractal_compute_value(f, i, j);
-			fractal_set_value(f, i, j, val);
-			fractal_set_value(f, f->width - i, f->height - j, val); 	/*par symétrie*/
-		}
-	}
+/*
+*void compute_value(struct fractal *f){
+*
+*	for(int i = 0; i < f->width/2; i++ ){
+*		for (int j = 0; j < f->height/2; j++){
+*			int val = fractal_compute_value(f, i, j);
+*			fractal_set_value(f, i, j, val);
+*			fractal_set_value(f, f->width - i, f->height - j, val); 	/*par symétrie*/
+/*		}
+*	}
+*
 }
+*/
