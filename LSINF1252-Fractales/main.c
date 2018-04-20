eb46495d4b4e8aecb9f328c26dea196c77684f93
Rodesc <rodolphe.deschaetzen.dev@gmail.com>
2018-04-20 12:09:41 +0200

    Test

--- LSINF1252-Fractales/main.c
+++ LSINF1252-Fractales/main.c
@@ -8,14 +8,16 @@ int main(){
 
     /* TODO */
 	printf("Creating fractal... \n");
-	struct fractal *f = fractal_new("Julia", 1920, 1080, 0.3, 0.5);
+	struct fractal *f = fractal_new("Julia", 192, 108, 0.3, 0.5);
 	printf("Fractal \"Julia\" created... \n");
 	printf("Computing \"Julia\"... \n");
 	compute_value(f);
 	printf("Computed \"Julia\"... \n Converting... \n");
 	int bmp = write_bitmap_sdl(f,"Julia1.bmp");
-	if(bmp == 0)
+	if(bmp == 0){
 		printf("\"Julia\" Converted to bmp file ! \n");
+		system("ristretto Julia1.bmp");
+	}
 	else
 		printf("Error while while creating bmp file format\n");
     return 0;
@@ -28,10 +30,10 @@ int main(){
 void compute_value(struct fractal *f){
 
 	for(int i = 0; i < f->width; i++ ){
-		for (int j = 0; j < f->height; j++){
+		for (int j = 0; j <= f->height/2; j++){
 			int val = fractal_compute_value(f, i, j);
 			fractal_set_value(f, i, j, val);
-			/*fractal_set_value(f, f->width - i, f->height - j, val); 	/*par symétrie*/
+			fractal_set_value(f, f->width - i, f->height - j, val); 	/*par symétrie*/
 		}
 	}
 }
